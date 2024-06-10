5e7efdc8e0dc5c133da58844e41f49fce6280115
Kostya Niekrasov <kostyanekrasov1501@gmail.com>
2024-06-10 17:15:01 +0300

    make build

---
+++ index.4a99735a.js
@@ -0,0 +1,2 @@
+window.addEventListener("hashchange",function(){"#menu"===window.location.hash?document.body.classList.add("page__body--with-menu"):document.body.classList.remove("page__body--with-menu")});var e=document.getElementById("contacts-fields");e.addEventListener("submit",function(t){t.preventDefault(),e.reset()});
+//# sourceMappingURL=index.4a99735a.js.map
